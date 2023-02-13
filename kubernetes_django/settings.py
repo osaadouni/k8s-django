f65ee30bc7eb19376748c8ff7df57cb78d1f6ddc
Administrator <admin@hackos.com>
2023-02-13 15:33:13 +0100

    Add django health check module

--- kubernetes_django/settings.py
+++ kubernetes_django/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Running Health Checks
+    'health_check',
+    'health_check.db',
 ]
 
 MIDDLEWARE = [
